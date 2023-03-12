e916f0b84130f0b4918ed1470c8a256c0159a3f0
Lal Ji <lalji5029@gmail.com>
2023-03-12 22:48:56 +0530

    java first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+class hello{
+     public static void main(String[] args) {
+        int a;
+        int b;
+        a=10;
+        b=30;
+System.out.println("hello lalji");    
+System.out.println("hii dear");   
+System.out.println(a+b); 
+System.out.println(!true);
+    }
+}
